6719dc5f8e88275aacf50fa82315f1569449e436
DevSatyamCollab <satyamkanungo0@gmail.com>
2025-12-22 18:13:48 +0000

    Feat: Implement consolePresenter and it's methods, Implement Base, add, delete, update, list, summary commands and it's methods

---
+++ internal/commands/commands.go
@@ -0,0 +1,229 @@
+package commands
+
+import (
+	"expense-tracker/internal/domain"
+	"expense-tracker/internal/presenter"
+	"expense-tracker/internal/service"
+	"fmt"
+)
+
+// ------------------
+// Icommand
+// ------------------
+type ICommand interface {
+	Validate() error
+	Execute() error
+}
+
+// ------------------
+// Base command
+// ------------------
+type Command struct {
+	service   *service.ExpenseService
+	presetner *presenter.ConsolePresenter
+}
+
+func newComamnd(s *service.ExpenseService, p *presenter.ConsolePresenter) *Command {
+	return &Command{
+		service:   s,
+		presetner: p,
+	}
+}
+
+// ------------------
+// Add command
+// ------------------
+type AddCommand struct {
+	Command
+	description string
+	category    string
+	amount      float64
+}
+
+func NewAddCommand(s *service.ExpenseService, p *presenter.ConsolePresenter, amount float64, desc, categ string) *AddCommand {
+	return &AddCommand{
+		Command:     *newComamnd(s, p),
+		description: desc,
+		category:    categ,
+		amount:      amount,
+	}
+}
+
+func (ac *AddCommand) Validate() error {
+	if err := domain.ValidateExpense(ac.amount, ac.description, ac.category); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (ac *AddCommand) Execute() error {
+	if err := ac.service.AddExpense(ac.amount, ac.description, ac.category); err != nil {
+		return err
+	}
+
+	ac.presetner.Success(fmt.Sprintf("Expense added successfully"))
+
+	return nil
+}
+
+// ------------------
+// Update command
+// ------------------
+type UpdateCommand struct {
+	Command
+	id          int
+	description string
+	category    string
+	amount      float64
+}
+
+func NewUpdateCommand(s *service.ExpenseService, p *presenter.ConsolePresenter, id int, amount float64, desc, categ string) *UpdateCommand {
+	return &UpdateCommand{
+		Command:     *newComamnd(s, p),
+		id:          id,
+		description: desc,
+		category:    categ,
+		amount:      amount,
+	}
+}
+
+func (uc *UpdateCommand) Validate() error {
+	if err := domain.ValidateID(uc.id); err != nil {
+		return err
+	}
+
+	if err := domain.ValidateExpense(uc.amount, uc.description, uc.category); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (uc *UpdateCommand) Execute() error {
+	if err := uc.service.UpdateExpense(uc.id, uc.amount, uc.description, uc.category); err != nil {
+		return err
+	}
+
+	uc.presetner.Success(fmt.Sprintf("Expense %d updated successfully", uc.id))
+
+	return nil
+}
+
+// -------------------
+// Delete command
+// -------------------
+type DeleteCommand struct {
+	Command
+	id int
+}
+
+func NewDeleteCommand(s *service.ExpenseService, p *presenter.ConsolePresenter, id int) *DeleteCommand {
+	return &DeleteCommand{
+		Command: *newComamnd(s, p),
+		id:      id,
+	}
+}
+
+func (dc *DeleteCommand) Validate() error {
+	if err := domain.ValidateID(dc.id); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (dc *DeleteCommand) Execute() error {
+	if err := dc.service.DeleteExpense(dc.id); err != nil {
+		return err
+	}
+
+	dc.presetner.Success(fmt.Sprintf("Expense %d deleted successfully", dc.id))
+
+	return nil
+}
+
+// -------------------
+// Summary command
+// -------------------
+type SummaryCommand struct {
+	Command
+	monthID  int
+	all      bool
+	category bool
+}
+
+func NewSummaryCommand(s *service.ExpenseService, p *presenter.ConsolePresenter, monthID int, category bool) *SummaryCommand {
+	return &SummaryCommand{
+		Command:  *newComamnd(s, p),
+		category: category,
+		monthID:  monthID,
+	}
+}
+
+func (sc *SummaryCommand) Validate() error {
+	if err := domain.ValidateMonthID(sc.monthID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sc *SummaryCommand) Execute() error {
+	switch {
+	case sc.monthID != 0:
+		sc.presetner.ShowSummaryofMonth(sc.monthID)
+	case sc.category:
+		sc.presetner.ShowSummaryofCategory()
+	default:
+		sc.presetner.ShowTotalSummary()
+	}
+
+	return nil
+}
+
+// -------------------
+// List Command
+// -------------------
+type ListCommand struct {
+	Command
+	monthID  int
+	category string
+}
+
+func NewListCommand(s *service.ExpenseService, p *presenter.ConsolePresenter, monthID int, category string) *ListCommand {
+	return &ListCommand{
+		Command:  *newComamnd(s, p),
+		monthID:  monthID,
+		category: category,
+	}
+}
+
+func (lc *ListCommand) Validate() error {
+	if lc.monthID != 0 {
+		if err := domain.ValidateMonthID(lc.monthID); err != nil {
+			return err
+		}
+	}
+
+	if lc.category != "" {
+		if err := domain.ValidateCategory(lc.category); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (lc *ListCommand) Execute() error {
+	switch {
+	case lc.monthID != 0:
+		lc.presetner.ShowList(lc.service.GetExpensesofMonth(lc.monthID))
+	case lc.category != "":
+		lc.presetner.ShowList(lc.service.GetExpensesByCategory(lc.category))
+	default:
+		lc.presetner.ShowList(lc.service.GetAllExpenes())
+	}
+
+	return nil
+}
---
+++ internal/presenter/console_presenter.go
@@ -0,0 +1,79 @@
+package presenter
+
+import (
+	"expense-tracker/internal/domain"
+	"expense-tracker/internal/service"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/aquasecurity/table"
+)
+
+const dateFormat string = "02-01-2006"
+
+// ---------------------
+// console presenter
+// ---------------------
+type ConsolePresenter struct {
+	service *service.ExpenseService
+}
+
+func NewConsolePresenter(s *service.ExpenseService) *ConsolePresenter {
+	return &ConsolePresenter{service: s}
+}
+
+// show success message
+func (p *ConsolePresenter) Success(msg string) {
+	fmt.Println(msg)
+}
+
+func (p *ConsolePresenter) ShowTotalSummary() {
+	price := p.service.GetTotalSummary()
+	fmt.Println(strconv.FormatFloat(price, 'f', 2, 64))
+}
+
+func (p *ConsolePresenter) ShowSummaryofMonth(monthID int) {
+	price := p.service.GetSummaryOfMonth(monthID)
+	month := time.Month(monthID).String()
+	fmt.Printf("Total expenses for %s: %.2f\n", month, price)
+}
+
+func (p *ConsolePresenter) ShowSummaryofCategory() {
+	categories := p.service.GetAllCategories()
+
+	if len(categories) == 0 {
+		fmt.Println("categories list is empty")
+		return
+	}
+
+	fmt.Println("SUMMARY OF CATEGORY:----------------------------------------------------\n")
+	tble := table.New(os.Stdout)
+	tble.SetHeaders("Category", "Price")
+
+	for _, category := range categories {
+		tble.AddRow(category, fmt.Sprintf("%.2f", p.service.GetSummaryByCategory(category)))
+	}
+
+	tble.Render()
+}
+
+func (p *ConsolePresenter) ShowList(expenseList []*domain.Expense) {
+	if len(expenseList) == 0 {
+		fmt.Println("list is empty")
+		return
+	}
+
+	fmt.Printf("Expense List:--------------------------------------------------------------\n")
+
+	tble := table.New(os.Stdout)
+	tble.SetHeaders("ID", "Date", "Description", "Amount", "Category")
+
+	for _, expense := range expenseList {
+		tble.AddRow(strconv.Itoa(expense.Id), expense.Date.Format(dateFormat),
+			expense.Description, strconv.FormatFloat(expense.Amount, 'f', 2, 64), expense.Category)
+	}
+
+	tble.Render()
+}
